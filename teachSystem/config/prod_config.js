ef580b190f6d5911bcbdd347658f504c2b2aedeb
fanxb <fanxb.tl@gmail.com>
2019-05-29 10:12:48 +0800

    删除无用文件

--- teachSystem/config/prod_config.js
+++
@@ -1,7 +0,0 @@
-const base = require("./base_config.js");
-const objectOperate = require("../util/ObjectOperate.js");
-const config = {
-  
-};
-
-module.exports = objectOperate.combineObject(base, config);
