790fe8bef4ed789be8547defd7e7ed13ff4dde76
fanxb <fanxb.tl@gmail.com>
2019-05-30 02:18:01 -0700

    fix:解决docker-compose 报错

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # mysql root密码通过环境变量配置
-# export MYSQL_PASS=123456
+#export MYSQL_PASS=123456
 # 判题服务密钥通过环境变量配置
-# export JUDGE_TOKEN = 12345678
+#export JUDGE_TOKEN=12345678
 
 #获取脚本所在目录
 path=$(cd `dirname $0`;pwd)
@@ -12,16 +12,17 @@ echo "Asia/Shanghai" >$path/timezone
 echo "mysql容器建立"
 
 #判断data中有没有teach_system数据库，没有就进行初始化数据库的操作
-omsCount=$(ls -l $path/env/mysql/data | grep -wc teach_system)
+omsCount=$(ls -l $path/files/mysql/data | grep -wc teach_system)
 if [ $omsCount == 0 ]; then
-    echo "首次安装系统，进行sql初始化,请稍候"
-    docker run --name=temp_mysql -itd -p 3306:3306 --privileged=true  -v $path/env/mysql/data:/var/lib/mysql  -v $path/env/mysql/my.cnf:/etc/mysql/my.cnf -v $path/env/mysql/init.sql:/opt/init.sql -e MYSQL_ROOT_PASSWORD=$MYSQL_PASS mysql:5.7.25
-    sleep 20s
-    docker exec mysql bash -c "mysql -u root -p$password</opt/init.sql"
+    echo "首次安装系统，进行sql初始化,请稍候,需等候一分钟左右"
+    docker stop temp_mysql
+    docker rm temp_mysql
+    docker run --name=temp_mysql -itd -p 3306:3306 --privileged=true  -v $path/files/mysql/data:/var/lib/mysql  -v $path/files/mysql/my.cnf:/etc/mysql/my.cnf -v $path/files/mysql/init.sql:/opt/init.sql -e MYSQL_ROOT_PASSWORD=$MYSQL_PASS mysql:5.7.25
+    sleep 50s
+    docker exec temp_mysql bash -c "mysql -u root -p$MYSQL_PASS</opt/init.sql"
     sleep 3s
     docker stop temp_mysql
     docker rm temp_mysql
 fi
 
-# 启动自己的
 docker-compose up -d
